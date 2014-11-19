06acb9282854320e3706dd92824f872bdeeb6879
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-11-18 20:57:03 -0600

    add feed

---
+++ cpanfile
@@ -0,0 +1,6 @@
+requires 'Web::Simple';
+requires 'IPC::System::Simple';
+requires 'File::pushd';
+requires 'IO::All';
+requires 'XML::Atom';
+requires 'DateTime';
---
+++ lib/Falcor2/Web.pm
@@ -0,0 +1,46 @@
+package Falcor2::Web;
+
+use 5.20.1;
+
+use experimental 'signatures', 'postderef';
+
+use Web::Simple;
+use warnings NONFATAL => 'all';
+no warnings::illegalproto;
+
+use IPC::System::Simple 'capture';
+use File::pushd;
+use IO::All;
+use XML::Atom::Feed;
+use XML::Atom::Entry;
+use XML::Atom::Content;
+use DateTime;
+
+
+sub dispatch_request {
+   GET => sub {
+      '/feed' => sub {
+         my $f = io->file($ENV{REMIND_PATH});
+         my $_d = pushd($f->absolute->filepath);
+
+         my $feed = XML::Atom::Feed->new;
+         $feed->title(q(Frew's Calendar));
+         my $entry = XML::Atom::Entry->new;
+         my $dt = DateTime->now->ymd('-');
+         $entry->title($dt);
+         $entry->id($dt);
+         my $c = capture('remind', $f->filename);
+         $entry->content(XML::Atom::Content->new(Type => 'xhtml', Body => "<pre>$c</pre>"));
+         $feed->add_entry($entry);
+
+         [ 200, [ 'Content-type', 'application/atom+xml' ], [$feed->as_xml] ]
+     },
+      '' => sub {
+         my $f = io->file($ENV{REMIND_PATH});
+         my $_d = pushd($f->absolute->filepath);
+         [ 200, [ 'Content-type', 'text/plain' ], [capture('remind', $f->filename) ] ]
+     },
+  }
+}
+
+1;
